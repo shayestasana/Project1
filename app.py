0b595715030994e9d919861eb16a751c8a9840d6
shayestasana <108970819+shayestasana@users.noreply.github.com>
2022-09-20 12:11:08 +0530

    a

--- app.py
+++ app.py
@@ -7,11 +7,14 @@ def index():
     return render_template('index.html')
 
 
-#froute
+# shortcut- froute
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
     
 if __name__ == '__main__':
-  app.run(host='127.0.0.1', port=8000, debug=True)
- 
+    app.run(host='127.0.0.1', port=8000, debug=True)
